dac62bf06e79a811aa749438886e762682e3b5e5
Noah Boeckmann <NoahBoeckmann@gmx.net>
2017-10-09 16:57:23 +0200

    Main lauffaehig gestaltet

--- src/de/emghaar/Main.java
+++ src/de/emghaar/Main.java
@@ -1,9 +1,11 @@
 package de.emghaar;
 import de.emghaar.game.Game;
+import de.emghaar.game.Human;
+
 public class Main {
-private Game game;
+private static Game game;
     public static void main(String[] args) {
 	// write your code here
-
+    game = new Game(new Human("Alex"), new Human("Noah"), new Human("Sebi"), new Human("Niggo"));
     }
 }
